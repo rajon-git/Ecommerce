bf8b32859248384877d21e743494951e83c0a754
rajon-git <rajon.zhsust15@gmail.com>
2023-02-21 22:47:33 +0600

    2nd commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,17 +3,17 @@ const jwt=require("jsonwebtoken");
 const {hashPassword,comparePassword}=require("../helpers/auth");
 
 //register
-const registerUser=async (req,res)=>{
+exports.register=async (req,res)=>{
     try{
-        const {name,email,password}=req.body;
+        const {name,email,password,address}=req.body;
         if(!name.trim()){
             return res.json({error:"Name is required"});
         }
         if(!email){
             return res.json({error:"Email is required"});
         }
-        if(!password){
-            return res.json({error:"Password is required"});
+        if(!password ||password.length<8){
+            return res.json({error:"Password must be more than 8 character"});
         }
         //Check if email is taken
         const existingUser=await User.findOne({email});
@@ -27,7 +27,8 @@ const registerUser=async (req,res)=>{
         const user=await new User({
             name,
             email,
-            password:hashedPassword
+            password:hashedPassword,
+            address
         }).save();
 
         //create signed token
@@ -46,4 +47,40 @@ const registerUser=async (req,res)=>{
         console.log(error);
     }
 }
-module.exports={registerUser};
+exports.login=async (req,res)=>{
+    try {
+        //destructured data from user
+        const {email, password} = req.body;
+        //varified aall fill
+        if(!email){
+            return res.json({error:"Email is required"});
+        }
+        if(!password){
+            return res.json({error:"Password is required"});
+        }
+        //check email is taken
+        const user=await User.findOne({email});
+        if(!user){
+            return res.json({error:"User not found"});
+        }
+        //match password
+        const match= await comparePassword(password,user.password);
+        if(!match){
+            return res.json({error:"Invalid email or password"});
+        }
+
+        //create signed token for user
+        const token=jwt.sign({_id:user._id},process.env.JWT_SECRET,{expiresIn:"7d"});
+        res.json({
+            user:{
+                name:user.name,
+                email:user.email,
+                role:user.role,
+                address: user.address
+            },
+            token
+        });
+    }catch(error){
+        console.log(error);
+    }
+}
